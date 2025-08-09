7bc3a76f07ff533e84461ebd3cf62ed4c7aca000
awrreny <awrreny@gmail.com>
2025-08-09 10:43:29 +0100

    added basic homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { Stack, Text, Title } from '@mantine/core';
+import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 
 export default function HomePage() {
   return (
-    <>
-      <Welcome />
+    <Stack justify="center" align="center" mx="auto" w="50%" ta={'center'}>
+      <Title mt="sm">
+        Welcome to{' '}
+        <Text span inherit variant="gradient" gradient={{ from: 'blue', to: 'cyan' }}>
+          u-win
+        </Text>
+        !
+      </Title>
+      <Text c="dimmed" size="md">
+        Unnamed Website thats Interesting and Nice
+      </Text>
+      <Text mt={30} size="lg">
+        A personal collection of CTF challenges - mainly in cryptography and reverse engineering.
+      </Text>
       <ColorSchemeToggle />
-    </>
+    </Stack>
   );
 }
