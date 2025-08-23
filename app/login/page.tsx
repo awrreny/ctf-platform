dd4627073c30b43056e8049f51e06b3f7453f761
awrreny <awrreny@gmail.com>
2025-08-23 16:08:13 +0100

    removed unused code in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
   // const callbackUrl = searchParams.get('callbackUrl') || '/';
 
   const form = useForm<LoginFormData>({
